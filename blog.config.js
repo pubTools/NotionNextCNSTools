b4c34b3c596e90264d948d070df0edce26957923
pubTools <ingopro@qq.com>
2024-08-03 18:09:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'CNSTools', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '祝人均CNS!', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.cnstools.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://cnstools.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'CNS, cnstools', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
